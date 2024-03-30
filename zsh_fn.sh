213e1b4dadecf50e7b151247d96c3e8cd7ad45fb
Jakub Vymola <kuba.vymola@gmail.com>
2024-03-30 09:57:00 +0100

    Setup npm realease action and mofify readme

--- zsh_fn.sh
+++ zsh_fn.sh
@@ -1,5 +1,5 @@
 c () {
-    node /Users/jakub/Programming/crrr/dist/cli.js "$1"
+    node /Users/jakub/Programming/crrr/dist/crrr.js "$1"
 
     if [ -f "/tmp/crrr" ]; then
         \builtin cd $(cat /tmp/crrr)
